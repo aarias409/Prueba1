dd555dd789406340f69ce39b0e1d4e1d43da813d
Sena <Sena@10.17.19.39>
2014-03-03 08:57:45 -0500

    03/03/2014 Incio del programa

---
+++ src/prueba1/Prueba1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+/**
+ *
+ * @author Sena
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
